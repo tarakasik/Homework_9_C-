025d4bffc9448a775a6fa2b5b75771f80d93f016
Stanislav Volokhov <lamantina123@gmail.com>
2022-09-05 17:12:14 +0400

    add #68

--- Task#64/Program.cs
+++ Task#64/Program.cs
@@ -5,44 +5,46 @@
 // M = 4; N = 8. -> ""4, 6, 7, 8""
 
 Console.Clear();
-Console.WriteLine("Input M : ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input N : ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-if (M <= 0 || N <= 0)
-{
-    Console.Write("The input number is not natural");
-}
-else
-{
-    Console.WriteLine("All natural numbers from M to N: ");
+Console.Write("Input number M:");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Input number N:");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("All natural numbers of M to N = "+SumNumbers(M,N));
 
+ string SumNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
     {
-        int range = 0;
-        while (M <= N)
-        {
-            M++;
-            range = M;
-            Console.Write(" ");
-            Console.Write($"{range - 1}");
-        }
+        return (start + " " + SumNumbers(start + 1, end));
     }
+
 }
 
-/////////Второй способ.
-// Console.Clear();
-// Console.Write("Input number M:");
-// int M = int.Parse(Console.ReadLine()!);
-// Console.Write("Input number N:");
-// int N = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("All natural numbers of M to N = "+SumNumbers(M,N));
 
-//  string SumNumbers(int start, int end)
+//// Второй вариант.
+
+//Console.Clear();
+// Console.WriteLine("Input M : ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input N : ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// if (M <= 0 || N <= 0)
 // {
-//     if (start == end) return start.ToString();
-//     {
-//         return (start + " " + SumNumbers(start + 1, end));
-//     }
+//     Console.Write("The input number is not natural");
+// }
+// else
+// {
+//     Console.WriteLine("All natural numbers from M to N: ");
 
+//     {
+//         int range = 0;
+//         while (M <= N)
+//         {
+//             M++;
+//             range = M;
+//             Console.Write(" ");
+//             Console.Write($"{range - 1}");
+//         }
+//     }
 // }
---
+++ Task#68/Program.cs
@@ -0,0 +1,21 @@
+﻿//// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+// A(0,n) = n+1
+// A(m,0) = A(m-1,1)
+// A(m,n) = A(m-1,A(m,n-1))
+
+Console.Clear();
+int Accerman(int m, int n)
+{
+    if (n == 0) return m + 1;
+    if (n > 0 && m == 0) return Accerman(1, n - 1);
+    return Accerman(Accerman(m - 1, n), n - 1);
+}
+
+Console.Write("Введите M: ");
+int numberM = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int numberN = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(Accerman(numberM, numberN));
